bcb561e977ef38f98c1bcd46e8836156682fce98
Anton Sjölander <anton.sjolander@hyperisland.se>
2016-12-19 19:54:56 +0100

    .

--- soundrops/main.js
+++ soundrops/main.js
@@ -238,6 +238,20 @@ screen.onfocus = function(){return false;}
 startScreen.onfocus = function(){return false;}
 $('body').bind('touchmove', function(event) { event.preventDefault() });
 
+function preventZoom(e) {
+  var t2 = e.timeStamp;
+  var t1 = e.currentTarget.dataset.lastTouch || t2;
+  var dt = t2 - t1;
+  var fingers = e.touches.length;
+  e.currentTarget.dataset.lastTouch = t2;
+
+  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+  e.preventDefault();
+  e.target.click();
+}
+
+body.addEventListener('touchstart', preventZoom);
 
 
 //Spacebar event 
